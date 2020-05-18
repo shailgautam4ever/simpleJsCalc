44cff7678f3c6aa8344d26a0b2733c8ebb3fa357
ROCKSTAR <rayskcoc@gmail.com>
2020-05-18 14:31:33 +0530

    'added'

--- app/index.js
+++ app/index.js
@@ -1,11 +1,41 @@
+let firstInput = '';
+let secondInput = '';
+let operation = '';
+let op = ['+', '-', '*', '/'];
+
 function update(value) {
-    //Type the code here.
+    if(!op.includes(value)){
+        if(!operation){
+            firstInput += value;
+            document.getElementById('screen').value = firstInput;
+        }else{
+            secondInput += value;
+            document.getElementById('screen').value =  firstInput + operation + secondInput;
+        }
+    }else{
+        operation = value;
+        document.getElementById('screen').value = firstInput + operation;
+    }
+    console.log('first', firstInput, 'second', secondInput, 'operation', operation);
 }
 
 function result() {
-    //Type the code here.
+    let result = 0;
+    if(operation == '+') result = +firstInput + +secondInput
+    else if(operation == '-') result = +firstInput - +secondInput
+    else if(operation == '*') result = +firstInput * +secondInput
+    else if(operation == '/') result = +firstInput / +secondInput
+
+    document.getElementById('screen').value = result;
+    firstInput = '';
+    secondInput = '';
+    operation = '';
 }
 
 function form_reset() {
-    //Type the code here.
+    firstInput = '';
+    secondInput = '';
+    operation = '';
+    document.getElementById('screen').value = '';
+    console.log('first', firstInput, 'second', secondInput, 'operation', operation);
 }
